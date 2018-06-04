c51b082d722f1b3c2b8007fbd8ec3e96213912f2
unknown <mattcmej@gmail.com>
2018-06-03 20:55:50 -0700

    Add express.static comment in server.js, add form in index.handlebars. Will put the content in a flexbox.

--- server.js
+++ server.js
@@ -2,11 +2,18 @@ var express = require("express");
 var bodyParser = require("body-parser");
 
 var app = express();
-var port = 3000;
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// Use the express.static middleware to serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
 
 var exphbs = require("express-handlebars");
 
@@ -18,8 +25,8 @@ var mysql = require("mysql");
 var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "",
-  database: "movie_planner_db"
+  password: "1@rv@1p10p519",
+  database: "burger_db"
 });
 
 connection.connect(function(err) {
@@ -32,5 +39,14 @@ connection.connect(function(err) {
 });
 
 app.get("/", function(req, res) {
-    
-})
+    connection.query("SELECT * FROM burgers;", function(err, data) {
+        if (err) return err;
+        res.render("index", {burgers: data});
+    });
+});
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
