6d109a2b4cb504a5e4ea7c090cf5518a60604548
unknown <mattcmej@gmail.com>
2018-06-06 15:37:26 -0700

    Add apiRoutes.js to help with routing, work on burger.js for front-end functionality (especially ajax requests), edit schema.sql.

--- app/public/assets/js/burger.js
+++ app/public/assets/js/burger.js
@@ -1,8 +1,73 @@
+$(document).ready(function() {
+
+    $(document).on("click", "button.eatBurger", eatBurger);
+
+
+    function eatBurger() {
+        event.stopPropagation();
+        var id = $(this).data("id");
+
+        // // Takes the info from the text box and places it 
+        // // in the burger list.
+        // var eatenBurger = {
+        //     burger_type: $("#burger-box").val().trim()
+        // };
+
+        var eatenBurger = {
+            burger_type: $(this).data("burger_type")
+        };
+
+        // Deletes the burger from the "burgers" table
+        $.ajax({
+            method: "DELETE",
+            url: "/api/burgers/" + id
+        })
+        .then(function() {
+
+            // Adds the burger to the "eaten_burgers" table
+            $.ajax({
+                method: "POST",
+                // data: eatenBurger,
+                // data: "/api/burgers" + id,
+                data: eatenBurger;
+                url: "/api/burgers"
+            })
+            .then(function() {
+                console.log("Ate the burger!");
+                location.reload();
+            })
+
+
+
+            // location.reload();
+        })
+    }
+
+
+
+
+
+
+
+
+})
+
+
+
+
+
+
+
+
+
 $(function() {
     $(".eatBurger").on("click", function(event) {
         var id = $(this).data("id");
 
         // DELETE request
-        $.ajax("api/burgers")
+        $.ajax({
+            method: "DELETE",
+            url: "/api/burgers" + id
+        }
     })
 })
--- schema.sql
+++ schema.sql
@@ -9,3 +9,11 @@ CREATE TABLE burgers (
     PRIMARY KEY (id)
 );
 
+CREATE TABLE eaten_burgers (
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_type VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO burgers (burger_type)
+VALUES ("Western Bacon"), ("Guacamole"), ("Hawaiian");
