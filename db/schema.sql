ee3a91baa83aa993dbe21996cc3fd55d3b49198b
unknown <mattcmej@gmail.com>
2018-06-09 09:59:55 -0700

    Split part of schema file into the seeds file.

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,4 @@
-DROP DATABASE IF EXISTS burger_db;
+-- DROP DATABASE IF EXISTS burger_db;
 CREATE DATABASE burger_db;
 
 USE burger_db;
@@ -9,12 +9,6 @@ CREATE TABLE burgers (
     eaten BOOLEAN DEFAULT false,
     PRIMARY KEY (id)
 );
-
--- CREATE TABLE eaten_burgers (
---     id INT NOT NULL AUTO_INCREMENT,
---     burger_type VARCHAR(30) NOT NULL,
---     PRIMARY KEY (id)
--- );
 
 INSERT INTO burgers (burger_type)
 VALUES ("Western Bacon"), ("Guacamole"), ("Hawaiian");
---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO burgers (burger_type)
+VALUES ("Western Bacon"), ("Guacamole"), ("Hawaiian");
